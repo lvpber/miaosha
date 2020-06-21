7a73e0247bbaa9972d80f9e9a05c3e63232ed7d0
lvpber <37864812+lvpber@users.noreply.github.com>
2020-06-21 20:47:22 +0800

    新机器测试

--- src/main/java/com/lvpb/miaosha/utils/MD5Util.java
+++ src/main/java/com/lvpb/miaosha/utils/MD5Util.java
@@ -7,7 +7,6 @@ import org.apache.commons.codec.digest.DigestUtils;
 public class MD5Util
 {
     private static final String salt = "1a2b3c4d";
-
     public static String md5(String src)
     {
         return DigestUtils.md5Hex(src);
