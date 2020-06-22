8cfe260b32fa875a49622e8844870dc1a08893f5
lvpengbo <lvpengbo@act.buaa.edu.cn>
2020-06-23 00:37:01 +0800

    添加rabbitmq相关配置

---
+++ src/main/java/com/lvpb/miaosha/config/MQConfig.java
@@ -0,0 +1,10 @@
+package com.lvpb.miaosha.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class MQConfig
+{
+}
---
+++ src/main/java/com/lvpb/miaosha/service/MQSender.java
@@ -0,0 +1,9 @@
+package com.lvpb.miaosha.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MQSender
+{
+
+}
